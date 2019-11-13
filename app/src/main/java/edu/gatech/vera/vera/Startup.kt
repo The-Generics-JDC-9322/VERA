3ecbc2c534f5db866ca42f4ddf575f8b40a09623
Isaac Dismukes <idismukes3@gatech.edu>
2019-11-12 19:36:12 -0500

    created application skeleton in android studio, began work on login screen

---
+++ app/src/main/java/edu/gatech/vera/vera/Startup.kt
@@ -0,0 +1,12 @@
+package edu.gatech.vera.vera
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Startup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_startup)
+    }
+}
