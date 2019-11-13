bc50e9bfe9f43acdd2385afad89596ba3276ed34
Isaac Dismukes <idismukes3@gmail.com>
2019-11-13 14:56:57 -0500

    added startup activity, began implementing select fitbit activity

---
+++ app/src/main/java/edu/gatech/vera/vera/Controllers/Startup.kt
@@ -0,0 +1,28 @@
+package edu.gatech.vera.vera.Controllers
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import edu.gatech.vera.vera.R
+
+import android.content.Intent
+import android.view.View
+import android.widget.Button
+
+class Startup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_startup)
+
+        val loginButton : Button = findViewById(R.id.loginButton)
+        loginButton.setOnClickListener {
+            onLoginClick(it)
+        }
+    }
+
+    private fun onLoginClick(view: View) {
+        // TODO: change transition animation
+        val intent = Intent(this, SelectFitbit::class.java)
+        startActivity(intent);
+    }
+}
