3da3901ab96c47cae5e6ffc202e7e69c66b53374
Isaac Dismukes <idismukes3@gmail.com>
2019-11-19 18:16:13 -0500

    added easter eggs

--- app/src/main/java/edu/gatech/vera/vera/Controllers/Monitoring.kt
+++ app/src/main/java/edu/gatech/vera/vera/Controllers/Monitoring.kt
@@ -2,10 +2,14 @@ package edu.gatech.vera.vera.Controllers
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Button
+import android.widget.Toast
 import edu.gatech.vera.vera.R
 import edu.gatech.vera.vera.model.devices.DeviceFactory
 import edu.gatech.vera.vera.model.devices.FitbitCloudDevice
+import kotlinx.android.synthetic.main.activity_monitoring.*
 
 class Monitoring : AppCompatActivity() {
 
@@ -18,9 +22,34 @@ class Monitoring : AppCompatActivity() {
         val fitbitId = findViewById<TextView>(R.id.fitbitId)
         fitbitId.text = fitbit.toString()
 
+        setupButtons()
+
         DeviceFactory.new()
             .ofType(FitbitCloudDevice::class)
             .named(fitbit.toString())
             .build()
+    }
+
+    private fun setupButtons() {
+        val monitoringButton = findViewById<Button>(R.id.monitoring)
+        val analyticsButton = findViewById<Button>(R.id.analytics)
+        val settingsButton = findViewById<Button>(R.id.settings)
+
+        monitoringButton.setOnClickListener {
+            val toast = Toast.makeText(this, "You're already on the Monitoring page!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+        analyticsButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Stay tuned for implementation!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+        settingsButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Stay tuned for implementation!", Toast.LENGTH_SHORT)
+            toast.show()
+//            val recordingStatus = findViewById<TextView>(R.id.recording_status)
+//            recordingStatus.visibility = View.VISIBLE
+        }
     }
 }
