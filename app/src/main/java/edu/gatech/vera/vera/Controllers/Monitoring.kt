15b5edaa9aac6200542e63109a0c8b9f462d26b3
Thomas Houghton <tomwhou@gmail.com>
2020-04-23 11:46:27 -0400

    VERA-18: VERA now POSTS to endpoint with heart rate data as well as officer data and geolocation.

--- app/src/main/java/edu/gatech/vera/vera/Controllers/Monitoring.kt
+++ app/src/main/java/edu/gatech/vera/vera/Controllers/Monitoring.kt
@@ -1,7 +1,10 @@
 package edu.gatech.vera.vera.Controllers
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -10,8 +13,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import edu.gatech.vera.vera.R
 import edu.gatech.vera.vera.model.Badge
 import edu.gatech.vera.vera.model.LocationService
@@ -23,10 +24,16 @@ import com.android.volley.toolbox.Volley
 import com.android.volley.RequestQueue
 import android.support.v4.app.SupportActivity
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.location.LocationManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.*
 import org.json.JSONObject
+import java.util.jar.Manifest
 
 
 class Monitoring : AppCompatActivity() {
@@ -62,20 +69,7 @@ class Monitoring : AppCompatActivity() {
      */
     private fun startListeningForHealthData() {
         val bpm = findViewById<TextView>(R.id.bpm)
-        val data = JSONObject("""{"geometry":{"type":"Point","coordinates":[-84.396431,33.778172]}, "properties":{"bpm":100}}""")
-        val url = "https://aqueous-falls-60920.herokuapp.com/hb/5000"
-        val request = JsonObjectRequest(Request.Method.POST,url,data,
-            Response.Listener { response ->
-                // Process the json
-                Log.d("Success", response.toString())
 
-            }, Response.ErrorListener{
-                // Error in request
-                Log.d("ERROR", "Post Request Error Response")
-            })
-        val requestQueue = Volley.newRequestQueue(this)
-        requestQueue.add(request)
-        Log.d("FINISHED", "!!!!!!!!!!!!!!!!!!")
         //define listener to set the bpm field text
         Monitor.listener = {
                 Log.d("Monitoring value", it.toString())
@@ -95,7 +89,7 @@ class Monitoring : AppCompatActivity() {
             .ofType(FitbitLocalhostDevice::class)
             .build()
 
-        Monitor.update()
+        Monitor.update(this)
 
     }
 
@@ -103,18 +97,47 @@ class Monitoring : AppCompatActivity() {
      * This function starts the location service and provides it with
      * location the last location found for processing.
      */
+    @SuppressLint("MissingPermission")
     private fun startLocationService() {
 
         //start LocationService
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location : Location? ->
-                // Got last known location. In some rare situations this can be null.
-                if (location != null) {
-                    LocationService.processLocation(location)
+        var locationL: Location? = null
+        val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        ActivityCompat.requestPermissions(this, permissions,0)
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0F, object : android.location.LocationListener {
+            override fun onLocationChanged(p0: Location?) {
+                if (p0 != null) {
+                    locationL = p0
+                    LocationService.processLocation(p0)
                 }
+                Log.d("Longitude", locationL!!.longitude.toString())
+                Log.d("Latitude", locationL!!.latitude.toString())
             }
+            override fun onProviderDisabled(p0: String?) {
+//
+            }
+
+            override fun onProviderEnabled(p0: String?) {
+//
+            }
+
+            override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+//
+            }
+        })
+//        fusedLocationClient.lastLocation
+//            .addOnSuccessListener { location : Location? ->
+//                // Got last known location. In some rare situations this can be null.
+//                locationL = location
+//                if (location != null) {
+//                    LocationService.processLocation(location)
+//                    Log.d("Location", "Also WORKING")
+//                    Log.d("LocationGPS", location.toString())
+//                }
+//            }
+//        Log.d("Location", locationL.toString())
     }
 
     private fun setupButtons() {
@@ -190,3 +213,4 @@ class Monitoring : AppCompatActivity() {
         startActivity(intent)
     }
 }
+
